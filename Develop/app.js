9d09b3700e38742a09e23da09ca04f1834474e4a
Gilberto Resendiz <gresendi@uci.edu>
2021-09-09 14:36:26 -0700

    created functions to create new eomployee classes based off user's choice

--- Develop/app.js
+++ Develop/app.js
@@ -1,7 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+const { prompt } = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
@@ -9,6 +9,147 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+let employees = []
+let employeeQuestion = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "Enter employee's name"
+
+  },
+  {
+    type: 'number',
+    name: 'id',
+    message: "Enter employee's id"
+
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Enter employee's email"
+
+  }
+
+]
+
+function createManager() {
+  let name = ''
+  let id = ''
+  let email = ''
+
+  prompt(employeeQuestion
+
+  ).then(({ name, id, email }) => {
+    name = name
+    id = id
+    email = email
+    prompt({
+      type: 'input',
+      name: 'officeNumber',
+      message: "What is their office number?"
+    }).then(({ officeNumber }) => {
+      let manager = new Manager(name, id, email, officeNumber)
+      employees.push(manager)
+      menu()
+    })
+
+  }).catch(err => console.error(err))
+
+
+}
+
+
+function createEngineer() {
+  let name = ''
+  let id = ''
+  let email = ''
+
+  prompt(employeeQuestion
+
+  ).then(({ name, id, email }) => {
+    name = name
+    id = id
+    email = email
+    prompt({
+      type: 'input',
+      name: 'github',
+      message: "What is their github username?"
+    }).then(({ github }) => {
+      let engineer = new Engineer(name, id, email, github)
+      employees.push(engineer)
+      menu()
+    })
+
+  }).catch(err => console.error(err))
+
+
+}
+
+function createIntern() {
+  let name = ''
+  let id = ''
+  let email = ''
+
+  prompt(employeeQuestion
+
+  ).then(({ name, id, email }) => {
+    name = name
+    id = id
+    email = email
+    prompt({
+      type: 'input',
+      name: 'github',
+      message: "What is their school named?"
+    }).then(({ github }) => {
+      let intern = new Intern(name, id, email, github)
+      employees.push(intern)
+      menu()
+    })
+
+  }).catch(err => console.error(err))
+
+
+}
+
+function menu(){
+  prompt({
+    type:'list',
+    name: 'employee',
+    message: 'Which type of employee would you like to add',
+    choices: ['Manager','Engineer','Intern','All Done']
+  }).then( ({employee})=>{
+    console.log(employee)
+    switch(employee){
+      case 'Manager':
+        createManager()
+        
+        break
+      case 'Engineer':
+        createEngineer()
+       
+        break
+      case 'Intern':
+        createIntern()
+        
+        break
+      case 'All Done':
+        render(employees)
+        break
+    }
+    
+  })
+  
+}
+
+function init(){
+
+  menu()
+  
+}
+init()
+// createManager()
+// createEngineer()
 
 
 // Write code to use inquirer to gather information about the development team members,
